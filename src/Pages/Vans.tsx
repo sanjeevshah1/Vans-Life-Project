c4ee5959ed0af3317de6d11822f4b18b4f8a5bcb
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-06-28 18:55:42 +0545

    updated Vans page

--- src/Pages/Vans.tsx
+++ src/Pages/Vans.tsx
@@ -1,7 +1,68 @@
-
+import "./../App.css"
+import van from "./../assets/van.png"
 const Vans = () => {
   return (
-    <h1>Vans page goes here 🚐</h1>
+   <div className="vans-container">
+      <h2>Explore our van options</h2>
+      <div className="filters flex">
+            <div className="buttons flex">
+              <button>Simple</button>
+              <button>Luxury</button>
+              <button>Rugged</button>
+            </div>
+            <button id="clear">Clear list</button>
+          </div>
+      <div className="actual-van-container">
+        <div className="van">
+          <img src={van} alt="" />
+          <div className="item-money">
+            <p>Modest Explorer</p>
+            <p>$60/day</p>
+          </div>
+          <button>Simple</button>
+        </div>
+        <div className="van">
+          <img src={van} alt="" />
+          <div className="item-money">
+            <p>Modest Explorer</p>
+            <p>$60/day</p>
+          </div>
+          <button>Rugged</button>
+        </div>
+        <div className="van">
+          <img src={van} alt="" />
+          <div className="item-money">
+            <p>Modest Explorer</p>
+            <p>$60/day</p>
+          </div>
+          <button>Simple</button>
+        </div>
+        <div className="van">
+          <img src={van} alt="" />
+          <div className="item-money">
+            <p>Modest Explorer</p>
+            <p>$60/day</p>
+          </div>
+          <button>Luxury</button>
+        </div>
+        <div className="van">
+          <img src={van} alt="" />
+          <div className="item-money">
+            <p>Modest Explorer</p>
+            <p>$60/day</p>
+          </div>
+          <button>Simple</button>
+        </div>
+        <div className="van">
+          <img src={van} alt="" />
+          <div className="item-money">
+            <p>Modest Explorer</p>
+            <p>$60/day</p>
+          </div>
+          <button>Luxury</button>
+        </div>
+      </div>
+   </div>
   )
 }
 
