e58127854a8d48ec4ca3d53fa1687e216e082544
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-06-28 17:20:30 +0545

    changed name

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import  {BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./Components/Home.tsx"
-import About from "./Components/About.tsx"
+import Home from "./Pages/Home.tsx"
+import About from "./Pages/About.tsx"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
