668859d48864940ae3b0f28552d74a25517e3395
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-06-29 12:48:57 +0545

    added variable colour to van buttons

--- src/Pages/Vans.tsx
+++ src/Pages/Vans.tsx
@@ -1,8 +1,9 @@
-import { useEffect,useState } from "react"
-import "./../App.css"
-import "./../../server.js"
+import { useEffect, useState } from "react";
+import "./../App.css";
+import "./../../server.js";
+
 const Vans = () => {
-  interface vansType{
+  interface VansType {
     description: string;
     id: string;
     imageUrl: string;
@@ -10,67 +11,81 @@ const Vans = () => {
     price: number;
     type: string;
   }
-  const [vans, setVans] = useState<vansType[]>([]);
+
+  const [vans, setVans] = useState<VansType[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<any>(null);
+
   useEffect(() => {
     const fetchVans = async () => {
-        try {
-            const response = await fetch('/api/vans');
-            console.log(response)
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const contentType = response.headers.get('content-type');
-            if (!contentType || !contentType.includes('application/json')) {
-                throw new TypeError("Received non-JSON response");
-            }
-            const data = await response.json();
-            setVans(data.vans);
-            console.log(data.vans)
-          } catch (error) {
-            setError(error)
-            console.error(error);
-          }finally{
-            setLoading(false);
-          }
-        };
+      try {
+        const response = await fetch("/api/vans");
+        console.log(response);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const contentType = response.headers.get("content-type");
+        if (!contentType || !contentType.includes("application/json")) {
+          throw new TypeError("Received non-JSON response");
+        }
+        const data = await response.json();
+        setVans(data.vans);
+        console.log(data.vans);
+      } catch (error) {
+        setError(error);
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-      fetchVans();
+    fetchVans();
   }, []);
 
-    if(loading){
-      return <div>Loading...</div>;
-    }
-    if(error){
-      return <div>Error: {error.message}</div>;
-    }
-
-    return(
-      <div className="vans-container">
-        <h2>Explore our van options</h2>
-        <div className="filters flex">
-             <div className="buttons flex">
-               <button>Simple</button>
-               <button>Luxury</button>
-               <button>Rugged</button>
-             </div>
-             <button id="clear">Clear list</button>
-        </div>
-        <div className="actual-van-container">
-          {vans.map((van) => (
-            <div className="van" key={van.id}>
-            <img src={van.imageUrl} alt={van.name} />
-            <div className="item-money">
-              <p>{van.name}</p>
-              <p>{van.price}/day</p>
-            </div>
-            <button>{van.type}</button>
-         </div>
-          ))
-        }   
-        </div>
-      </div>
-    );
+  if (loading) {
+    return <div>Loading...</div>;
   }
-export default Vans
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className="vans-container">
+      <h2>Explore our van options</h2>
+      <div className="filters flex">
+        <div className="buttons flex">
+          <button>Simple</button>
+          <button>Luxury</button>
+          <button>Rugged</button>
+        </div>
+        <button id="clear">Clear list</button>
+      </div>
+      <div className="actual-van-container">
+        {vans.map((van) => {
+          let backgroundColor:string = "";
+          if (van.type === "simple") {
+            backgroundColor = "#E17654";
+          } else if (van.type === "rugged") {
+            backgroundColor = "#115E59";
+          } else {
+            backgroundColor = "#161616";
+          }
+
+          return (
+            <div className="van" key={van.id}>
+              <img src={van.imageUrl} alt={van.name} />
+              <div className="item-money">
+                <p>{van.name}</p>
+                <p>${van.price}/day</p>
+              </div>
+              <button style={{backgroundColor}}>{van.type.charAt(0).toUpperCase() + van.type.slice(1)}</button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Vans;
