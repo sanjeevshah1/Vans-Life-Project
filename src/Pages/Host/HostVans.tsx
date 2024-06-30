25eeaf67493d486c1ef3d0b1dfb3b3069bf260f6
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-06-30 21:45:38 +0545

    added Host Vans page

--- src/Pages/Host/HostVans.tsx
+++ src/Pages/Host/HostVans.tsx
@@ -1,7 +1,36 @@
-
+import van from "./../../assets/van.png"
 const HostVans = () => {
   return (
-    <div>Vans</div>
+    <div className="listed-vans">
+            <div className="head">
+                <p>Your Listed Vans</p>
+                <p>View all</p>
+            </div>
+            <div className="van-box">
+                <img src={van} alt="van" />
+                <div className="van-explain">
+                    <p>Modest Explorer</p>
+                    <p>$60/day</p>
+                </div>
+                <p>Edit</p>
+            </div>
+            <div className="van-box">
+                <img src={van} alt="van" />
+                <div className="van-explain">
+                    <p>Modest Explorer</p>
+                    <p>$60/day</p>
+                </div>
+                <p>Edit</p>
+            </div>
+            <div className="van-box">
+                <img src={van} alt="van" />
+                <div className="van-explain">
+                    <p>Modest Explorer</p>
+                    <p>$60/day</p>
+                </div>
+                <p>Edit</p>
+            </div>
+        </div>
   )
 }
 
