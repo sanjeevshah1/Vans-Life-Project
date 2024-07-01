8a8b33836018427c29d35bec2b238aaa31bd8b81
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-07-01 13:44:50 +0545

    trying new branch

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -5,6 +5,7 @@ const Home = () => {
    <>
         <div className="intro-container both-center">
             <div className="intro">
+                <h1>Just Checking</h1>
                 <h2>You got the travel plans,
                     we  <br /> got the travel vans.</h2>
                 <p>Add adventure to your life by joining the #vanlife movement.<br />  Rent the perfect van to make your perfect road trip.</p>
