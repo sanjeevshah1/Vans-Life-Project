83e98d671da64d07b84e53598dba9e67948082e3
sanjeevshah1 <sahsanjeev42@gmail.com>
2024-07-02 11:17:24 +0545

    Design maintenance of Host Van Detail page

--- src/Pages/Host/Details.tsx
+++ src/Pages/Host/Details.tsx
@@ -2,7 +2,15 @@
 
 const Details = () => {
   return (
-    <div>Details</div>
+    <div className="detail-container">
+      <p><span>Name:</span> Modest Explorer</p>
+      <br />
+      <p><span>Category:</span> Simple</p>
+      <br />
+      <p><span>Description:</span> The Modest Explorer is a van designed to get you out of the house and into nature. This beauty is equipped with solar panels, a composting toilet, a water tank and kitchenette. The idea is that you can pack up your home and escape for a weekend or even longer!</p>
+      <br />
+      <p><span>Visibility:</span>Public</p>
+    </div>
   )
 }
 
